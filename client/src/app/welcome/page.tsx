cf3709da915ab021ca0f10b2fd7dd6ba8dc71e3d
Alex <acim650@gmail.com>
2023-12-26 20:24:41 -0800

    added statsbox

--- client/src/app/welcome/page.tsx
+++ client/src/app/welcome/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Col, Divider, Row } from 'antd';
 
 const { Header, Content, Sider } = Layout;
 
@@ -76,6 +76,21 @@ const App: React.FC = () => {
                     >
                         Content
                     </Content>
+                    <Divider orientation="left">Responsive</Divider>
+                    <Row>
+                        <Col span={6} xs={{ order: 1 }} sm={{ order: 2 }} md={{ order: 3 }} lg={{ order: 4 }}>
+                            1 col-order-responsive
+                        </Col>
+                        <Col span={6} xs={{ order: 2 }} sm={{ order: 1 }} md={{ order: 4 }} lg={{ order: 3 }}>
+                            2 col-order-responsive
+                        </Col>
+                        <Col span={6} xs={{ order: 3 }} sm={{ order: 4 }} md={{ order: 2 }} lg={{ order: 1 }}>
+                            3 col-order-responsive
+                        </Col>
+                        <Col span={6} xs={{ order: 4 }} sm={{ order: 3 }} md={{ order: 1 }} lg={{ order: 2 }}>
+                            4 col-order-responsive
+                        </Col>
+                    </Row>
                 </Layout>
             </Layout>
         </Layout>
