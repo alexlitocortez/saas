1cee5f58499dc4bd4dd7a59b65772ffda0cf012d
Alex <acim650@gmail.com>
2024-03-23 21:17:48 -0700

    fixed env variable

--- client/src/app/auth-callback/page.tsx
+++ client/src/app/auth-callback/page.tsx
@@ -7,9 +7,6 @@ const Page = async () => {
     const router = useRouter()
 
     const searchParams = useSearchParams()
-    const origin = searchParams.get('origin')
-
-    const apiResponse = await fetch('/api/whatever')
 
     const { data, error } = trpc.authCallback.useQuery();
 
