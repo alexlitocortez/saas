789e387d1b858653b24b84ae6ce79acc37da4ba8
Alex <acim650@gmail.com>
2024-03-26 21:52:48 -0700

    fixed errors on dashboard page and added providers to layout

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,15 +1,17 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server"
-import { redirect } from "next/dist/server/api-utils"
+import { redirect } from "next/navigation"
 import Dashboard from "src/components/Dashboard"
 
 const Page = async () => {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
 
+    const
+
 
     // If userData is null, redirect to auth callback with a URL
     if (!user || !user.id) {
-        redirect('/auth-callback?origin=dashboard')
+        redirect('/auth-callback?origin=dashboard');
     }
 
     // return <div>{user && user.email}</div>
